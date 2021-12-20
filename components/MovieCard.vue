d79c35c4f44c21aad281a535058217229cf73505
mhasanince <mhasanince@gmail.com>
2021-12-20 16:48:21 +0300

    fix: date string format

--- components/MovieCard.vue
+++ components/MovieCard.vue
@@ -40,7 +40,9 @@ export default {
         : 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No-Image-Placeholder.svg/1665px-No-Image-Placeholder.svg.png'
     },
     updatedAt() {
-      return this.movie.release_date.replaceAll('-', '/')
+      return this.movie.release_date
+        ? this.movie.release_date.replaceAll('-', '/')
+        : ''
     },
   },
 }
