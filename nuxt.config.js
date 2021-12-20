4dab753d06664ffe1dbe7e552e8529f69267f20b
mhasanince <mhasanince@gmail.com>
2021-12-20 16:17:16 +0300

    update head meta

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,6 +10,8 @@ export default {
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
       { name: 'format-detection', content: 'telephone=no' },
+      { name: 'http-equiv', content: 'Content-Security-Policy' },
+      { name: 'content', content: 'upgrade-insecure-requests' },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
